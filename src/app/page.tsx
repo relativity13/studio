14881fac473263ef21b7f87aed0a2896397f09b0
Harshit Agarwal <harshit1110@gmail.com>
2025-07-19 14:54:59 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section id="testi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Footer } from "@/components/landing/footer";
 import { Header } from "@/components/landing/header";
 import { Hero } from "@/components/landing/hero";
 import { Products } from "@/components/landing/products";
-import { Testimonials } from "@/components/landing/testimonials";
 
 export default function Home() {
   return (
@@ -12,7 +11,6 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <Products />
-        <Testimonials />
         <Cta />
       </main>
       <Footer />
